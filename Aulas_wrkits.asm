a46f2a434a8ac0a889d088b5be5ca73ec3d8c0ec
Julius <julius.romanzeira@gmail.com>
2023-02-18 20:09:37 -0300

    Mudança, agora se trata de aulas da UFPE

--- Aulas_wrkits.asm
+++ Aulas_wrkits.asm
@@ -1,4 +1,22 @@
 ;
-;	Estudo com Wagner Ramos da WRkits
+;	Estudo UFPE - Professor Doutor Marcio Evaristo Da Cruz Brito
+;
+;	Aluno: Julius Romanzeira
+;	Curso: Eng. Controle e Automação
 ;
 
+
+processor	16f877a		; Definição do processador alvo: PIC 16F877A
+radix		DEC			; Definição dos decimais como padrão no programa
+
+
+#include <p16f877a.inc>		; Inclusão de arquivo código-fonte
+
+	org 00h		; Definição do endereço de origem
+	goto inicio		; Mandando o programa ir para o label inicio
+	
+
+inicio:		; Label inicio
+
+	banksel TRISD
+	movlw	
