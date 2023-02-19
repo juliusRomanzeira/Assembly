402eb03bc38e7912fef1281600cd4c926455d82a
Julius <julius.romanzeira@gmail.com>
2023-02-19 12:57:38 -0300

    Melhoramento do programa 1

--- Program/Aulas_wrkits.asm
+++ Program/Aulas_wrkits.asm
@@ -28,13 +28,29 @@ inicio:		; Label inicio
 	movlw	00000000b	; Todos são saida
 	movwf	PORTD
 
+	
+PISCA_LEDS:		; Label de loop, onde o programa irá funcionar
 
 	
-loop:		; Label de loop, onde o programa irá funcionar
-
-
-	bsf		PORTD,0		; Setando a porta 1 do PORTD
 	
-	goto loop		; Vai mandar o programa para o label loop
+	bsf		PORTD,0
+	bcf		PORTD,0
+	bsf		PORTD,1
+	bcf		PORTD,1
+	bsf		PORTD,2
+	bcf		PORTD,2
+	bsf		PORTD,3
+	bcf		PORTD,3
+	bsf		PORTD,4
+	bcf		PORTD,4
+	bsf		PORTD,5
+	bcf		PORTD,5
+	bsf		PORTD,6
+	bcf		PORTD,6
+	bsf		PORTD,7
+	bcf		PORTD,7
+	
+	
+	goto PISCA_LEDS		; Vai mandar o programa para o label PISCA_LEDS
 	
 	end				; Finaliza o programa
