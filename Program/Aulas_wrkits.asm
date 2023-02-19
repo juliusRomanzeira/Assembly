3d45d1829a5ae7e833f8799db5a82c757ff56f62
Julius <julius.romanzeira@gmail.com>
2023-02-19 11:47:43 -0300

    Criação da pasta Program e finalização do programa 1

--- Program/Aulas_wrkits.asm
+++ Program/Aulas_wrkits.asm
@@ -15,16 +15,26 @@ radix		DEC			; Definição dos decimais como padrão no programa
 	org 00h		; Definição do endereço de origem
 	goto inicio		; Mandando o programa ir para o label inicio
 	
+ 
 
 inicio:		; Label inicio
 
 	banksel TRISD		; Selecionando o banco de memória com o registrador trisd
 	movlw	00000000b	; "Setando" todos os pinos do TRISD como saida ("1" Entrada "0" Saida)
-	movwf TRISD			; Jogando o valor para TRISD
+	movwf	TRISD		; Jogando o valor para TRISD
 	
+	
+	banksel PORTD		; Selecionando a porta D
+	movlw	00000000b	; Todos são saida
+	movwf	PORTD
+
+
 	
 loop:		; Label de loop, onde o programa irá funcionar
 
+
+	bsf		PORTD,0		; Setando a porta 1 do PORTD
+	
 	goto loop		; Vai mandar o programa para o label loop
 	
 	end				; Finaliza o programa
