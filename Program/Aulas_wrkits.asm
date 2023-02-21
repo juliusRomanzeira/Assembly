f6cc411bd9263d2c6f08ee5e40714cb392ec2a00
Julius <julius.romanzeira@gmail.com>
2023-02-21 18:43:33 -0300

    Atraso de 1.628600 mSegundos

--- Program/Aulas_wrkits.asm
+++ Program/Aulas_wrkits.asm
@@ -2,7 +2,7 @@
 ;	Estudo UFPE - Professor Doutor Marcio Evaristo Da Cruz Brito
 ;
 ;	Aluno: Julius Romanzeira
-;	Curso: Eng. Controle e Automação
+;	Curso: Eng. Controle e Automaï¿½ï¿½o
 ;
 
 
@@ -12,7 +12,7 @@ radix		DEC			; Definição dos decimais como padrão no programa
 
 #include <p16f877a.inc>		; Inclusão de arquivo código-fonte
 
-	org 00h		; Definição do endereço de origem
+	org 0x00		; Definição do endereço de origem
 	goto inicio		; Mandando o programa ir para o label inicio
 	
  
@@ -28,12 +28,25 @@ inicio:		; Label inicio
 	movlw	00000000b	; Todos são saida
 	movwf	PORTD
 
+
+
+
+
+; --- Label de Variável ---
+
+	org 0x20		; Endereço que eu quero usar para registrar minha variável
+temp	res 1 	; Reservando 1 bit de memï¿½ria para a variável
+
+
+
+
+	
+; --- Label que faz os leds do TRISD pircarem ---
 	
 PISCA_LEDS:		; Label de loop, onde o programa irá funcionar
 
-	
-	
 	bsf		PORTD,0
+	call	Atraso
 	bcf		PORTD,0
 	bsf		PORTD,1
 	bcf		PORTD,1
@@ -49,8 +62,23 @@ PISCA_LEDS:		; Label de loop, onde o programa irá funcionar
 	bcf		PORTD,6
 	bsf		PORTD,7
 	bcf		PORTD,7
-	
-	
+		
 	goto PISCA_LEDS		; Vai mandar o programa para o label PISCA_LEDS
+	
+	
+	
+	
+	
+; --- Label de atraso para redução da frequência de piscadas dos leds ---
+
+Atraso:
+
+	movlw	1
+	movwf	temp
+	
+	
+@1	decf	temp,f
+	btfss	STATUS,Z	; Vai analizar se o registrador z estão ou não setado. Página 19 do DATASHEET	
+	goto @1
 	
 	end				; Finaliza o programa
